e81d5e20d4325d97a01adb552ae87fc69a5cd001
LinRenJie <xoxosos666@gmail.com>
2023-09-02 18:08:16 +0800

    :fire: 模版React+tailwind+ts

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+/*
+ * @Author: LinRenJie xoxosos666@gmail.com
+ * @Date: 2023-08-29 23:46:59
+ * @Description:
+ */
+import './App.css'
+import Header from './components/Header'
+
+const App = () => {
+  return (
+    <div className="bg-site ">
+      <Header />
+    </div>
+  )
+}
+
+export default App
